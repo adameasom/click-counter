ed95c7af9cb1afe7a4efd3f3ce229a99107fe802
Adam Measom <adameasom@mail.com>
2024-12-19 00:49:24 +0900

    Reset button enabled when count at 0

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ function ClickCounter() {
       <button
         onClick={handleResetHighScore}
         className="reset-button"
-        disabled={isActive || timeLeft < 10}
+        disabled={isActive}
       >
         Reset
       </button>
